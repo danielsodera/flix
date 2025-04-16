5d8b467fb4aa3b3221ede3d90a5bdb9a5ef2b40c
Danielsodera <daniel_sodera@me.com>
2025-04-16 13:02:55 -0400

    removed uniqueness validation from movie title since it was causing issues when uploading to Heroku

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,7 @@ class Movie < ApplicationRecord
   has_many :characterizations, dependent: :destroy 
   has_many :genres, through: :characterizations
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
   validates :released_on, :duration, presence: true 
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
